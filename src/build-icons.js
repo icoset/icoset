a016888a389ba7a758267e2fe453b3397268f585
Tony Lefler <tony.lefler@canopytax.com>
2018-10-07 15:35:49 -0600

    skip unparsable svgs and warn user about it

--- src/build-icons.js
+++ src/build-icons.js
@@ -16,26 +16,29 @@ module.exports = function buildIcons(iconList = [], { svgoPlugins = {} } = {}) {
         plugins: Object.keys(svgoPlugins)
           .map(key => ({ [key]: svgoPlugins[key] }))
       });
-      svgo.optimize(file).then(optimizedFile => {
-        file = optimizedFile.data;
-        let viewBox = '';
-        if (file.includes('viewBox')) {
-          const viewBoxAttr = file.match(RegExp('viewBox="([^"]*)"'))[0];
-          if (viewBoxAttr) {
-            viewBox = viewBoxAttr.slice(0, viewBoxAttr.length - 1);
-            viewBox = viewBox.slice(viewBox.indexOf('"') + 1);
-          }
-        }
-        viewBoxMap[icon.name] = { viewBox };
-        file = file
-          .replace(/svg>/g, 'symbol>')
-          .replace(/<svg/g, '<symbol')
-          .replace('<symbol', `<symbol id="${icon.name}"`);
+      svgo.optimize(file)
+        .then(optimizedFile => {
+            file = optimizedFile.data;
+            let viewBox = '';
+            if (file.includes('viewBox')) {
+              const viewBoxAttr = file.match(RegExp('viewBox="([^"]*)"'))[0];
+              if (viewBoxAttr) {
+                viewBox = viewBoxAttr.slice(0, viewBoxAttr.length - 1);
+                viewBox = viewBox.slice(viewBox.indexOf('"') + 1);
+              }
+            }
+            viewBoxMap[icon.name] = { viewBox };
+            file = file
+              .replace(/svg>/g, 'symbol>')
+              .replace(/<svg/g, '<symbol')
+              .replace('<symbol', `<symbol id="${icon.name}"`);
 
-        resolveSvgo(file);
-      }, (err) => {
-        throw Error(err);
-      });
+            resolveSvgo(file);
+          },
+          (err) => {
+            console.warn(`[icoset] Unable to parse svg: "${icon.path}"`);
+            console.warn('skipped file');
+          });
     });
   }));
 
